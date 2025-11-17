61704fd33cbe55b952460873a5041122deb6d46e
Seb Ospina <kraige@gmail.com>
2025-11-17 13:49:56 +0100

    signal name changes

--- src/app.rs
+++ src/app.rs
@@ -10,32 +10,32 @@ extern "C" {
 }
 
 #[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+struct ReplaysDirectory<'a> {
+    path: &'a str,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (name, set_name) = signal(String::new());
-    let (greet_msg, set_greet_msg) = signal(String::new());
+    let (replay_path_r, replay_path_w) = signal(String::new());
+    let (check_replay_path_r, check_replay_path_w) = signal(String::new());
 
     let update_name = move |ev| {
         let v = event_target_value(&ev);
-        set_name.set(v);
+        replay_path_w.set(v);
     };
 
-    let greet = move |ev: SubmitEvent| {
+    let set_replays_path = move |ev: SubmitEvent| {
         ev.prevent_default();
         spawn_local(async move {
-            let name = name.get_untracked();
+            let name = replay_path_r.get_untracked();
             if name.is_empty() {
                 return;
             }
 
-            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name }).unwrap();
+            let args = serde_wasm_bindgen::to_value(&ReplaysDirectory { path: &name }).unwrap();
             // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
             let new_msg = invoke("greet", args).await.as_string().unwrap();
-            set_greet_msg.set(new_msg);
+            check_replay_path_w.set(new_msg);
         });
     };
 
@@ -53,15 +53,15 @@ pub fn App() -> impl IntoView {
             </div>
             <p>"Click on the Tauri and Leptos logos to learn more."</p>
 
-            <form class="row" on:submit=greet>
+            <form class="row" on:submit=set_replays_path>
                 <input
                     id="greet-input"
-                    placeholder="Enter a name..."
+                    placeholder="Enter the directory name..."
                     on:input=update_name
                 />
-                <button type="submit">"Greet"</button>
+                <button type="submit">"Set Replays Path"</button>
             </form>
-            <p>{ move || greet_msg.get() }</p>
+            <p>{ move || check_replay_path_r.get() }</p>
         </main>
     }
 }
