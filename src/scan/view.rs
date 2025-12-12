fc889027a3ee1b8cd7eabfa03f7582362f60d84c
Seb Ospina <kraige@gmail.com>
2025-12-12 22:36:54 +0100

    Changing icon maybe...

--- src/scan/view.rs
+++ src/scan/view.rs
@@ -1,9 +1,192 @@
 //! Swarmy Tauri UI - Scan View
-use super::*;
-use crate::scan::SC2ReplaysDirStatsTable;
+
+use crate::*;
+use leptos::ev::MouseEvent;
+use leptos::leptos_dom::logging::console_log;
 use leptos::prelude::*;
+use leptos::task::spawn_local;
+use phosphor_leptos::{Icon, IconWeight, CPU};
 use reactive_graph::traits::Read;
-use reactive_stores::Store;
+use reactive_graph::traits::Write;
+use reactive_stores::{Patch, Store};
+use s2protocol::SC2ReplaysDirStats;
+use swarmy_tauri_common::*;
+
+#[component]
+pub fn ScanDirectory() -> impl IntoView {
+    let (replay_path, set_replay_path) = signal(String::new());
+    let (scan_button_enabled, set_scan_button_enabled) = signal(false);
+    let (disable_parallel_scans, set_disable_parallel_scans) = signal(false);
+
+    spawn_local(async move {
+        let args = serde_wasm_bindgen::to_value(&AppSettings {
+            disable_parallel_scans: disable_parallel_scans.get_untracked(),
+            replay_paths: vec![replay_path.get_untracked()],
+        })
+        .unwrap();
+        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+        match serde_wasm_bindgen::from_value::<AppSettings>(
+            invoke("get_current_app_config", args).await,
+        ) {
+            Ok(config) => {
+                console_log(&format!("Loaded app config: {:?}", config));
+                set_disable_parallel_scans.set(config.disable_parallel_scans);
+                if let Some(path) = config.replay_paths.first() {
+                    console_log(&format!("Loading path: {:?}", path));
+                    set_replay_path.set(path.clone());
+                    set_scan_button_enabled.set(true);
+                }
+            }
+            Err(e) => {
+                console_log(&format!("Error invoking basic_scan_replay_path: {:?}", e));
+                set_scan_button_enabled.set(true);
+                return;
+            }
+        }
+    });
+    let tx_update_replay_dir = move |ev| {
+        let v = event_target_value(&ev);
+        set_scan_button_enabled.set(v.len() > 0);
+        set_replay_path.set(v);
+    };
+    let fake_data = SC2ReplaysDirStats {
+        total_files: 0,
+        total_supported_replays: 0,
+        ability_supported_replays: 0,
+        top_10_maps: vec![],
+        top_10_players: vec![],
+    };
+    let fake_data_table: SC2ReplaysDirStatsTable = fake_data.into();
+    let data = Store::new(fake_data_table);
+
+    let trigger_basic_scan_replay_path = move |ev: MouseEvent| {
+        ev.prevent_default();
+        let name = replay_path.get_untracked();
+        let disable_parallel = disable_parallel_scans.get_untracked();
+        if name.is_empty() {
+            console_log("Replay path is empty.");
+            return;
+        }
+        set_scan_button_enabled.set(false);
+
+        spawn_local(async move {
+            let args = serde_wasm_bindgen::to_value(&AppSettings {
+                disable_parallel_scans: disable_parallel,
+                replay_paths: vec![name],
+            })
+            .unwrap();
+            // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+            match serde_wasm_bindgen::from_value::<SC2ReplaysDirStats>(
+                invoke("basic_scan_replay_path", args).await,
+            ) {
+                Ok(stats) => {
+                    set_scan_button_enabled.set(true);
+                    let mut stats_table: SC2ReplaysDirStatsTable = stats.into();
+                    console_log(&format!("New data = {:?}", stats_table));
+                    data.top_10_players().write().retain(|_| false);
+                    data.top_10_players()
+                        .write()
+                        .append(&mut stats_table.top_10_players);
+                    data.top_10_maps().write().retain(|_| false);
+                    data.top_10_maps()
+                        .write()
+                        .append(&mut stats_table.top_10_maps);
+                    data.total_files().patch(stats_table.total_files);
+                    data.total_supported_replays()
+                        .patch(stats_table.total_supported_replays);
+                    data.ability_supported_replays()
+                        .patch(stats_table.ability_supported_replays);
+                }
+                Err(e) => {
+                    console_log(&format!("Error invoking basic_scan_replay_path: {:?}", e));
+                    set_scan_button_enabled.set(true);
+                    return;
+                }
+            }
+        });
+    };
+
+    view! {
+        <div class="grid grid-cols-8 grid-rows-1 gap-1">
+            <div class="col-span-5">
+                <label class="input input-sm w-full">
+                    "Path"
+                    <input
+                        class="input input-sm my-0 mx-0"
+                        id="scan-directory-input"
+                        value=move || replay_path.get()
+                        on:input=tx_update_replay_dir
+                        type="text"
+                    />
+                </label>
+            </div>
+            <div class="col-span-2 justify-start">
+                <button
+                    class={move || if replay_path.get().len() > 0 { "btn btn-primary btn-sm m-0" } else { "btn btn-disabled btn-sm m-0" }}
+                    on:click=trigger_basic_scan_replay_path
+                    disabled= {move || !scan_button_enabled.get() }
+                    title="Initial scanfor StarCraft II replays">
+                    {
+                        move || if replay_path.get().len() > 0{
+                            "Scan"
+                        } else if scan_button_enabled.get() {
+                            "Scanning..."
+                        } else {
+                            "Scan"
+                        }
+                    }
+                </button>
+                <button
+                    class={move || if replay_path.get().len() > 0 { "btn btn-success btn-sm m-0" } else { "btn btn-disabled btn-sm m-0" }}
+                    on:click=trigger_basic_scan_replay_path
+                    disabled= {move || !scan_button_enabled.get() }
+                    title="Optimize the replay generating Arrow files (may take some time)">
+                    {
+                        move || if replay_path.get().len() > 0 && !scan_button_enabled.get() {
+                            "Optimizing..."
+                        } else {
+                            "Optimize"
+                        }
+                    }
+                </button>
+            </div>
+            <div class="col-span-1 flex justify-end">
+                <label class="label cursor-pointer" title="Disable Parallel Processing (less CPU usage)">
+                    <input type="checkbox"
+                        prop:class=move || if disable_parallel_scans.get() {
+                            "btn btn-soft my-0 mx-0 btn-active"
+                        } else {
+                            "btn btn-soft my-0 mx-0"
+                        }
+                        checked=move || disable_parallel_scans.get()
+                        on:click=move |_| set_disable_parallel_scans.set(!disable_parallel_scans.get()) />
+                        <span
+                        title=move || if disable_parallel_scans.get() {
+                            "Parallel Processing Disabled"
+                        } else {
+                            "Parallel Processing Enabled"
+                        }
+                        prop:class=move || if disable_parallel_scans.get() {
+                            "label-text text-success mx-1"
+                        } else {
+                            "label-text text-warning-content mx-1"
+                        } >
+                        <Icon icon=CPU weight=IconWeight::Bold
+                            color=move || if disable_parallel_scans.get() {
+                                "red"
+                            } else {
+                                "green"
+                            }
+                            />
+                        </span>
+                </label>
+            </div>
+        </div>
+        <ReplayScanTable
+            data
+         />
+    }
+}
 
 #[component]
 pub fn ReplayScanTable(data: Store<SC2ReplaysDirStatsTable>) -> impl IntoView {
