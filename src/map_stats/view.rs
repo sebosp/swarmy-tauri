e17c7966ae29b4bc86c0f5b38f9721edc23b6f52
Seb Ospina <kraige@gmail.com>
2026-02-08 21:37:50 +0100

    A bit cleaner tailwind css

--- src/map_stats/view.rs
+++ src/map_stats/view.rs
@@ -1,158 +1,156 @@
 //! Leptos view for map stats.
+
+use super::*;
 use crate::*;
-use leptos::leptos_dom::logging::console_log;
 use leptos::prelude::*;
-use leptos::task::spawn_local;
 use reactive_stores::Store;
-use serde::{Deserialize, Serialize};
-use swarmy_tauri_common::*;
-
-use s2protocol::details::PlayerLobbyDetails;
-
-#[derive(Store, Debug, Default, Clone, Serialize, Deserialize)]
-pub struct MapStatsDataFrame {
-    pub total: usize,
-    #[store(key: String = |row| row.title.clone())]
-    pub res: Vec<PlayerLobbyDetails>,
-    pub start: usize,
-    pub end: usize,
-    pub page: usize,
-    pub per_page: usize,
-}
-
-async fn fetch_query_map_stats(
-    query: MapStatsQuery,
-) -> Result<ApiResponse, SwarmyTauriError> {
-    let args = serde_wasm_bindgen::to_value(&query).unwrap();
-    console_log(&format!(
-        "Invoking fetch_query_map_stats with args: {:?}",
-        args
-    ));
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    let response =
-        serde_wasm_bindgen::from_value::<ApiResponse>(invoke("query_map_stats", args).await)?;
-    Ok(response)
-}
-
-fn trigger_fetch_query_map_stats(
-    data: Store<MapStatsDataFrame>,
-    query: ReadSignal<MapStatsQuery>,
-    set_backend_response: WriteSignal<ApiResponse>,
-) {
-    let query_cp = query.get_untracked();
-    spawn_local(async move {
-        console_log("Fetching map stats...");
-        match fetch_query_map_stats(query_cp).await{
-            Err(e) => {
-                console_log(&format!("Error fetching map stats: {:?}", e));
-                *set_backend_response.write() = ApiResponse {
-                    meta: ResponseMeta::incomplete(),
-                    message: format!("Error fetching map stats: {:?}", e),
-                };
-            }
-            Ok(response) => {
-                *set_backend_response.write() = response.clone();
-                *data.write() = serde_json::from_str(&response.message).unwrap_or_default();
-            }
-        }
-    });
-}
 
 #[component]
 pub fn StatsByMap() -> impl IntoView {
-    let (query, set_query) = signal(MapStatsQuery::default());
-    let (player_name, set_player_name) = signal(String::new());
-    let (map_title, set_map_title) = signal(String::new());
-    let (map_stats, set_map_stats) = signal(MapStatsDataFrame::default());
+    let player_name = RwSignal::new(String::new());
+    let map_title = RwSignal::new(String::new());
+    let query = move || MapStatsQuery {
+        map_title: map_title.get(),
+        player_name: player_name.get(),
+    };
     let (backend_response, set_backend_response) = signal(ApiResponse {
         meta: ResponseMeta::incomplete(),
         message: String::new(),
     });
-    let map_stats_data = Store::new(MapStatsDataFrame::default());
-    trigger_fetch_query_map_stats(map_stats_data, query, set_backend_response);
+    let map_stats_store = Store::new(MapStatsDataTable::default());
+    trigger_fetch_query_map_stats(map_stats_store, query(), set_backend_response);
     view! {
-        <div>
-            <div class="grid grid-cols-8 grid-rows-1 gap-1">
-                <div class="col-span-3">
-                    <label class="input input-sm">
-                        <span class="label">"Map"</span>
-                        <input
-                            class="input input-sm my-0 mx-0"
-                            value=move || map_title.get()
-                            on:input=move |_| {
-                                trigger_fetch_query_map_stats(
-                                    map_stats_data,
-                                    query,
-                                    set_backend_response,
-                                );
-                            }
-                            type="text"
-                        />
-                    </label>
+        <div class="grid grid-cols-10 gap-1">
+            <div class="col-span-4">
+                <label for="map_title" class="block text-sm/6 font-medium text-white">
+                    Map
+                </label>
+                <div>
+                    <input
+                        name="map_title"
+                        class=text_input_tailwind_classes().join(" ")
+                        bind:value=map_title
+                        on:input=move |_| {
+                            trigger_fetch_query_map_stats(
+                                map_stats_store,
+                                query(),
+                                set_backend_response,
+                            );
+                        }
+                        type="text"
+                    />
                 </div>
-                <div class="col-span-1"></div>
-                <div class="col-span-3">
-                    <label class="input input-sm">
-                        <span class="label">"Player"</span>
-                        <input
-                            class="input input-sm my-0 mx-0"
-                            value=move || player_name.get()
-                            on:input=move |_| {
-                                trigger_fetch_query_map_stats(
-                                    map_stats_data,
-                                    query,
-                                    set_backend_response,
-                                );
-                            }
-                            type="text"
-                        />
-                    </label>
-                </div>
-                <div class="col-span-1"></div>
             </div>
-            <DisplayBackendStatus backend_response />
-            <Show when=move || { map_stats.get().total > 0 }>
-                <MapStatsDataTable map_stats_data />
+            <div class="col-span-1"></div>
+            <div class="col-span-4">
+                <label for="player_name" class="block text-sm/6 font-medium text-white">
+                    Player
+                </label>
+                <div>
+                    <input
+                        name="player_name"
+                        class=text_input_tailwind_classes().join(" ")
+                        bind:value=player_name
+                        on:input=move |_| {
+                            trigger_fetch_query_map_stats(
+                                map_stats_store,
+                                query(),
+                                set_backend_response,
+                            );
+                        }
+                        type="text"
+                    />
+                </div>
+            </div>
+            <div class="col-span-1"></div>
+        </div>
+        <DisplayBackendStatus backend_response />
+        <div class="col-span-10">
+            <h2 class="text-base/7 font-semibold text-white">
+                {move || map_stats_store.total().get()} " Unique maps found in snapshot."
+            </h2>
+            <Show when=move || { map_stats_store.total().get() > 0 }>
+                <MapStatsDataTable map_stats_store />
             </Show>
         </div>
     }
 }
 
 #[component]
-pub fn MapStatsDataTable(map_stats_data: Store<MapStatsDataFrame>) -> impl IntoView {
+pub fn MapStatsDataTable(map_stats_store: Store<MapStatsDataTable>) -> impl IntoView {
     view! {
-        <div class="overflow-x-auto w-full p-2">
-            <table class="table w-full">
-                <thead>
-                    <tr>
-                        <th>"Map Title"</th>
-                        <th>"Games Played"</th>
-                        <th>"Average Players"</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <For
-                        each=move || map_stats_data.res()
-                        key=|row| {
-                            format!(
-                                "{}:{}",
-                                row.read().ext_fs_id.clone(),
-                                row.read().user_init_data_name.clone(),
-                            )
-                        }
-                        children=|child| {
-                            view! {
-                                <tr>
-                                    <td>{child.read().title.clone()}</td>
-                                    <td>{child.read().ext_datetime.to_string()}</td>
-                                    <td>{child.read().user_init_data_clan_tag.clone()}</td>
-                                    <td>{child.read().user_init_data_name.clone()}</td>
-                                </tr>
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table class="relative min-w-full divide-y divide-white/15">
+                    <thead>
+                        <tr>
+                            <th
+                                scope="col"
+                                class="py-3.5 pr-3 pl-4 text-left text-sm font-semibold whitespace-nowrap sm:pl-0 text-white"
+                            >
+                                "Map Title"
+                            </th>
+                            <th
+                                scope="col"
+                                class="px-2 py-3.5 text-left text-sm font-semibold whitespace-nowrap text-white"
+                            >
+                                "Games"
+                            </th>
+                            <th
+                                scope="col"
+                                class="px-2 py-3.5 text-left text-sm font-semibold whitespace-nowrap text-white"
+                            >
+                                "Cache Handles"
+                            </th>
+                            <th
+                                scope="col"
+                                class="px-2 py-3.5 text-left text-sm font-semibold whitespace-nowrap text-white"
+                            >
+                                "Min Date"
+                            </th>
+                            <th
+                                scope="col"
+                                class="px-2 py-3.5 text-left text-sm font-semibold whitespace-nowrap text-white"
+                            >
+                                "Max Date"
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-white/10 bg-gray-900">
+                        <For
+                            each=move || map_stats_store.data()
+                            key=|row| {
+                                format!("{}:{}", row.read().title.clone(), row.read().cache_handles)
                             }
-                        }
-                    />
-                </tbody>
-            </table>
+                            children=|child| {
+                                let handles_count = child
+                                    .read()
+                                    .cache_handles
+                                    .split(",")
+                                    .collect::<Vec<&str>>()
+                                    .len();
+                                view! {
+                                    <tr>
+                                        <td class="py-2 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-white">
+                                            {child.read().title.clone()}
+                                        </td>
+                                        <td class="px-2 py-2 text-sm whitespace-nowrap text-gray-400">
+                                            {child.read().num_games}
+                                        </td>
+                                        <td>{handles_count}</td>
+                                        <td class="px-2 py-2 text-sm whitespace-nowrap text-gray-400">
+                                            {format!("{}", child.read().min_date)}
+                                        </td>
+                                        <td class="px-2 py-2 text-sm whitespace-nowrap text-gray-400">
+                                            {format!("{}", child.read().max_date)}
+                                        </td>
+                                    </tr>
+                                }
+                            }
+                        />
+                    </tbody>
+                </table>
+            </div>
         </div>
     }
 }
