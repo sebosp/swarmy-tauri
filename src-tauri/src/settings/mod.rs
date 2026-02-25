6fa6b1ce08458f6a210d233b3dc0baa69379c879
Seb Ospina <kraige@gmail.com>
2026-02-25 20:08:40 +0100

    Load the store from the app_handle easier to use

--- src-tauri/src/settings/mod.rs
+++ src-tauri/src/settings/mod.rs
@@ -1,12 +1,15 @@
 //! Module for application settings management.
 use swarmy_tauri_common::*;
-use tauri_plugin_store::Store;
 
 use crate::try_get_snapshot_metadata;
+use tauri_plugin_store::StoreBuilder;
 
-pub async fn load_app_settings_from_store<R: tauri::Runtime>(
-    store: &Store<R>,
-) -> Result<AppSettings, SwarmyTauriError> {
+pub async fn load_app_settings(app_handle: tauri::AppHandle) -> Result<AppSettings, SwarmyTauriError> {
+    let store = StoreBuilder::new(&app_handle, "settings.json")
+        .build()?;
+
+    // If there are no saved settings yet, this will return an error so we ignore the return value.
+    let _ = store.reload();
     let disable_parallel_scans = store
         .get("disable_parallel_scans")
         .and_then(|v| v.as_bool())
